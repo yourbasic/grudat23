485ef6e892f0b59bd89d54c4ca268f0079c62b4b
Stefan Nilsson <korthaj@users.noreply.github.com>
2019-03-11 15:16:38 +0100

    Create uppg2.py

---
+++ ovn0/uppg2.py
@@ -0,0 +1,12 @@
+# Nisse Nilsson, grudat19 uppg 0.2
+
+def odd(n):
+	"""Tell if n is an odd number."""
+	return n%2 != 0
+
+# Unit test
+assert not odd(-2)
+assert odd(-1)
+assert not odd(0)
+assert odd(1)
+assert not odd(2)
